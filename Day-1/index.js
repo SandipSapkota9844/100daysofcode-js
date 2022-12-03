0ae95a64903e87a09d29ec48e4d50a39ca1cf507
Sandip Sapkota <sandipsapkota9844@gmail.com>
2022-12-04 00:34:24 +0545

    Day-1

---
+++ Day-1/index.js
@@ -0,0 +1,23 @@
+const author = 'Sandip'
+const Publisher = "Ramarujan"
+console.log(author)
+
+var ram = 0 ;
+let hari;
+let sandip = BigInt("5674") + BigInt("3") ;
+console.log (sandip)
+
+const sita = Symbol("hi");
+console.log(hari);
+console.log (typeof(sita));
+
+// objects 
+
+
+ const ram = {
+   "HUHU" : true,
+   "Sita" : 56
+
+ }
+
+console.log(ram["Sita"]);
