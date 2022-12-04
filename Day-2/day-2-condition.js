db675b0dc45eb5f7b16cd0977d15fbdeff891668
Sandip Sapkota <sandipsapkota9844@gmail.com>
2022-12-04 23:24:39 +0545

    conditionals expressions

---
+++ Day-2/day-2-condition.js
@@ -0,0 +1,23 @@
+//Conditionals expressions in javascripts;
+
+let age = parseInt(prompt("Hey What's your age?"));
+if (age < 18)
+ {
+  console.log("You are not allowed for voting for being minor");
+}
+else if(age>100)
+{
+console.log("You are not allowed to vote as you are over aged ");
+}
+else{
+console.log("You are allowed to vote");
+};
+
+
+
+//HomeWork - Switch Case;
+
+
+
+
+
