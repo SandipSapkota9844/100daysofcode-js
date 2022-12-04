57d4c3af3afe5bec5edb8e7b30fff69ef40b13f6
Sandip Sapkota <sandipsapkota9844@gmail.com>
2022-12-04 23:02:59 +0545

    operators in js

---
+++ Day-2/day-2.js
@@ -0,0 +1,41 @@
+// Airthmetic operators in
+// let a = 40;
+// let b = 6;
+console.log ("a+b is ", a+b);
+console.log ("a-b is ", a-b);
+console.log ("a*b is ", a*b);
+console.log ("a/b is ", a/b);
+console.log ("a**b is ", a**b);
+console.log ("a%b is ", a%b);
+console.log ("a++ is ", a++);
+console.log ("a-- is ", a--);
+
+//Assignment Operators
+
+let s = 11;
+let r = 10;
+console.log("r+s is", (s += r));
+console.log("r-s is", (s -= r));
+console.log("r*s is", (s *= r));
+console.log("r/s is", (s /= r));
+console.log("r%s is", (s %= r));
+console.log("r**s is", (s **= r));
+
+
+//comparsion operator
+
+let value1=100;
+let value2="100";
+
+    console.log(value1==value2);// gives true cause here the vaule is same as == operator checks only vaule stored in variable
+  console.log(value1===value2);//gives false cause here the value is same but the type is different one is string and another is number.
+  console.log(value1!=value2) // will gives false cause it only check value no types
+  console.log(value1!==value2) // give true as used for checking value as well as type of the vaule
+
+
+
+  //logical operators
+  let sandip =99;
+  let sapkota=89;
+console.log(sandip>sapkota && sapkota<sandip);
+
